bf16295048000037aecd3dc8ce5e5bf84351c935
sammy-code98 <sammysmart98@yahoo.com>
2021-11-16 17:59:28 +0100

    feat:add recipe section

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Works from "./components/Works/Works";
 import Welcome from "./components/Welcome/Welcome";
+import Recipe from "./components/Recipe/Recipe";
 function App() {
   return (
     <Router>
       <GlobalStyles />
-      <Navbar /> 
+      <Navbar />
       <Hero />
       <Works />
       <Welcome />
+      <Recipe />
     </Router>
   );
 }
---
+++ src/components/Recipe/Recipe.js
@@ -0,0 +1,213 @@
+import React from "react";
+import { OutlineButton } from "../../GlobalStyles";
+import SalmonImg from "../../images/pizza.jpg";
+import ChickenImg from "../../images/hero.jpg";
+import PizzaImg from "../../images/hero.jpg";
+import PastaImg from "../../images/pizza.jpg";
+import SaladImg from "../../images/hero.jpg";
+
+import {
+  RecipeContainer,
+  RecipeWrapper,
+  RecipeTitle,
+  RecipeContentContainer,
+  RecipeTabContainer,
+  RecipeBtn,
+  RecipeCardWrapper,
+  RecipeFeature,
+  RecipeFeatureContent,
+  RecipeFeatureTitle,
+  RecipeFeatureText,
+  RecipeFeatureDetails,
+  RecipeFeatureItem,
+  RecipeItemTitle,
+  RecipeItemContent,
+  RecipeItemIcon,
+  RecipeItemText,
+  RecipeCardSection,
+  RecipeSmallCards,
+  RecipeCard,
+  RecipeCardContent,
+  RecipeCardHeading,
+  RecipeCardDetails,
+  RecipeCardItems,
+  RecipeCardTitle,
+  RecipeCardItem,
+  RecipeCardIcon,
+  RecipeCardText,
+  RecipeImg,
+  Img,
+} from "./Recipe.styles";
+function Recipe() {
+  return (
+    <div>
+      <RecipeWrapper>
+        <RecipeContainer>
+          <RecipeTitle>Recipes</RecipeTitle>
+          <RecipeContentContainer>
+            <RecipeTabContainer>
+              <RecipeBtn to="/sea-food">
+                <OutlineButton big bigFont bigRadius>
+                  Sea Food
+                </OutlineButton>
+              </RecipeBtn>
+              <RecipeBtn to="/Vegetarian">
+                <OutlineButton big bigFont bigRadius>
+                  Vegetarian
+                </OutlineButton>
+              </RecipeBtn>
+              <RecipeBtn to="/meat">
+                <OutlineButton big bigFont bigRadius>
+                  Meat and Poultry
+                </OutlineButton>
+              </RecipeBtn>
+              <RecipeBtn to="/easy-preps">
+                <OutlineButton big bigFont bigRadius>
+                  Easy meal preps
+                </OutlineButton>
+              </RecipeBtn>
+            </RecipeTabContainer>
+            <RecipeCardWrapper>
+              <RecipeFeature>
+                <RecipeImg src={SalmonImg} alt="Salmon Recipe" />
+                <RecipeFeatureContent>
+                  <RecipeFeatureTitle>
+                    Salmon and Hot Honey Butter
+                  </RecipeFeatureTitle>
+                  <RecipeFeatureText>
+                    The hot honey kinda sounds like a hype name used before the
+                    20s, (I used it back then). It’s a perfect coating for the
+                    salmon to enrich it with sweetness and heat.
+                  </RecipeFeatureText>
+                  <RecipeFeatureDetails>
+                    <RecipeFeatureItem>
+                      <RecipeItemTitle>Serving</RecipeItemTitle>
+                      <RecipeItemContent>
+                        <RecipeItemIcon />
+                        <RecipeItemText>2</RecipeItemText>
+                      </RecipeItemContent>
+                    </RecipeFeatureItem>
+                    <RecipeFeatureItem>
+                      <RecipeItemTitle>Cook time</RecipeItemTitle>
+                      <RecipeItemText>35-45 min</RecipeItemText>
+                    </RecipeFeatureItem>
+                    <RecipeFeatureItem>
+                      <RecipeItemTitle>Difficulty level</RecipeItemTitle>
+                      <RecipeItemText>20%</RecipeItemText>
+                    </RecipeFeatureItem>
+                  </RecipeFeatureDetails>
+                </RecipeFeatureContent>
+              </RecipeFeature>
+              <RecipeCardSection>
+                <RecipeSmallCards>
+                  <RecipeCard>
+                    <Img src={ChickenImg} alt="Chicken Recipe" />
+                    <RecipeCardContent>
+                      <RecipeCardHeading>
+                        Pretzel-Crusted Chicken
+                      </RecipeCardHeading>
+                      <RecipeCardDetails>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Serving</RecipeCardTitle>
+                          <RecipeCardItem>
+                            <RecipeCardIcon />
+                            <RecipeCardText>2</RecipeCardText>
+                          </RecipeCardItem>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Cook time</RecipeCardTitle>
+                          <RecipeCardText>30-45 min</RecipeCardText>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Difficulty level</RecipeCardTitle>
+                          <RecipeCardText>20%</RecipeCardText>
+                        </RecipeCardItems>
+                      </RecipeCardDetails>
+                    </RecipeCardContent>
+                  </RecipeCard>
+                  <RecipeCard>
+                    <Img src={SaladImg} alt="Salad Recipe" />
+                    <RecipeCardContent>
+                      <RecipeCardHeading>Sesame Asian Salad</RecipeCardHeading>
+                      <RecipeCardDetails>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Serving</RecipeCardTitle>
+                          <RecipeCardItem>
+                            <RecipeCardIcon />
+                            <RecipeCardText>2</RecipeCardText>
+                          </RecipeCardItem>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Cook time</RecipeCardTitle>
+                          <RecipeCardText>10-15 min</RecipeCardText>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Difficulty level</RecipeCardTitle>
+                          <RecipeCardText>10%</RecipeCardText>
+                        </RecipeCardItems>
+                      </RecipeCardDetails>
+                    </RecipeCardContent>
+                  </RecipeCard>
+                </RecipeSmallCards>
+                <RecipeSmallCards>
+                  <RecipeCard>
+                    <Img src={PizzaImg} alt="Pizza Recipe" />
+                    <RecipeCardContent>
+                      <RecipeCardHeading>
+                        Italian Pizza with Cheese
+                      </RecipeCardHeading>
+                      <RecipeCardDetails>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Serving</RecipeCardTitle>
+                          <RecipeCardItem>
+                            <RecipeCardIcon />
+                            <RecipeCardText>2</RecipeCardText>
+                          </RecipeCardItem>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Cook time</RecipeCardTitle>
+                          <RecipeCardText>30-45 min</RecipeCardText>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Difficulty level</RecipeCardTitle>
+                          <RecipeCardText>20%</RecipeCardText>
+                        </RecipeCardItems>
+                      </RecipeCardDetails>
+                    </RecipeCardContent>
+                  </RecipeCard>
+                  <RecipeCard>
+                    <Img src={PastaImg} alt="Pasta Recipe" />
+                    <RecipeCardContent>
+                      <RecipeCardHeading>
+                        Pasta with creamy sause
+                      </RecipeCardHeading>
+                      <RecipeCardDetails>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Serving</RecipeCardTitle>
+                          <RecipeCardItem>
+                            <RecipeCardIcon />
+                            <RecipeCardText>2</RecipeCardText>
+                          </RecipeCardItem>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Cook time</RecipeCardTitle>
+                          <RecipeCardText>10-15 min</RecipeCardText>
+                        </RecipeCardItems>
+                        <RecipeCardItems>
+                          <RecipeCardTitle>Difficulty level</RecipeCardTitle>
+                          <RecipeCardText>10%</RecipeCardText>
+                        </RecipeCardItems>
+                      </RecipeCardDetails>
+                    </RecipeCardContent>
+                  </RecipeCard>
+                </RecipeSmallCards>
+              </RecipeCardSection>
+            </RecipeCardWrapper>
+          </RecipeContentContainer>
+        </RecipeContainer>
+      </RecipeWrapper>
+    </div>
+  );
+}
+
+export default Recipe;
